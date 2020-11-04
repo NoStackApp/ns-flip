772f197f003e2916672f2c827c3974b2881b26c2
YizYah <yyakovson613@gmail.com>
2020-11-04 13:33:31 +0200

    added general to context and to nsInfo

---
+++ src/codeGeneration/context/createGeneralInfo.ts
@@ -0,0 +1,16 @@
+import {NsInfo} from '../../constants/types/nsInfo'
+
+const fs = require('fs-extra')
+
+export async function createGeneralInfo(nsInfo: NsInfo, codeDir: string) {
+  let general: any = {}
+  if (nsInfo.general)
+    general = {...nsInfo.general}
+
+  const codePackageJsonPath = `${codeDir}/package.json`
+  if (await fs.pathExists(codePackageJsonPath)) {
+    const codePackageJson = await fs.readJson(codePackageJsonPath)
+    general.json = codePackageJson
+  }
+  return general
+}
